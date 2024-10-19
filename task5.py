725968467a722b3835ac26e258690e0df3d06d26
Vlados <vladprkrf@mail.ru>
2024-10-19 13:46:56 +0700

    Первые 5 задач решены

--- task5.py
+++ task5.py
@@ -4,3 +4,12 @@
 # Пользоваться input()[::-1] запрещено!
 # Идея задачи реализовать алгоритм,
 # который будет работать для любого введенного натурального числа.
+def unuseful_and_forgotten_sad_function_because_slices_are_exist(a):
+    mad_str = a % 10
+    if a//10 == 0:
+        return str(mad_str)
+    else:
+        return str(mad_str) + unuseful_and_forgotten_sad_function_because_slices_are_exist(int((a - mad_str)/10))
+
+
+print(unuseful_and_forgotten_sad_function_because_slices_are_exist(int(input())))
