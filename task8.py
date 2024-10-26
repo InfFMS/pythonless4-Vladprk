911148b65472667af069514d299476d972423fc6
Vlados <vladprkrf@mail.ru>
2024-10-26 13:37:41 +0700

    исправлена задача 8

--- task8.py
+++ task8.py
@@ -1,12 +1,25 @@
 # Напишите рекурсивную функцию, которая
 # раскладывает натуральное число на простые сомножители.
-def func_razlozeni(num):
-    for i in range(2, int(num**0.5) + 1):
-        while num % i == 0:
-            print(i)
-            num //= i
-    if num != 1:
-        print(num)
+def func_razlozeni(num, i):
+    def prostoe(n):
+        delit = 0
+        for j in range(1, int((n**0.5)+2)):
+            if n % j == 0:
+                delit += 1
+        if delit == 1:
+            return True
+        else:
+            return False
+
+    while num % i == 0:
+        print(i)
+        num //= i
+    if not prostoe(num):
+        i += 1
+        func_razlozeni(num, i)
+    else:
+        if num != 1:
+            print(num)
 
 
-func_razlozeni(int(input()))
+func_razlozeni(int(input()), 2)
